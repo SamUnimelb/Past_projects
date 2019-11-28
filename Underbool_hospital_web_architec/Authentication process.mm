248e738f05ce982b848a39311d5dc44dfe69079c
Sam Yan <896298116@qq.com>
2019-11-28 20:18:34 +1100

    Added files
    Added files about the Underbool web arch did with Mr. Frank Smolener.

---
+++ Underbool_hospital_web_architec/Authentication process.mm
@@ -0,0 +1,26 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1521526778062" ID="ID_218229039" MODIFIED="1521527894529" TEXT="U2F when first log in">
+<node CREATED="1521526816115" ID="ID_1962684570" MODIFIED="1521526858674" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Access desktop computers in doctor's offices
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1521527958588" ID="ID_122432584" MODIFIED="1521528015763" TEXT="Tap NFC smart card or"/>
+<node CREATED="1521528019162" ID="ID_1371365139" MODIFIED="1521528023177" TEXT="Input password"/>
+</node>
+<node CREATED="1521527878609" ID="ID_1988817394" MODIFIED="1521527922619" POSITION="right" TEXT="Access desktop computers from ED / ICU">
+<node CREATED="1521527971084" ID="ID_508594220" MODIFIED="1521527976439" TEXT="Tap NFC smart card or"/>
+<node CREATED="1521527979781" ID="ID_1924538522" MODIFIED="1521527999560" TEXT="Access through Ex-sight&apos;s facial recognition system"/>
+</node>
+<node CREATED="1521526877936" ID="ID_690115665" MODIFIED="1521526908203" POSITION="right" TEXT="Access desktop computers in patient&apos;s ward"/>
+<node CREATED="1521527905491" ID="ID_926555030" MODIFIED="1521527915767" POSITION="right" TEXT="Access desktop computers from operating room"/>
+</node>
+</map>
---
+++ Underbool_hospital_web_architec/Minor report 2/Zoom webinar atendee flow.mm
@@ -0,0 +1,14 @@
+<map version="0.8.0 RC5">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1523526947010" ID="Freemind_Link_1935740038" MODIFIED="1523526958053" TEXT="Start computer">
+<node CREATED="1523526971239" ID="_" MODIFIED="1523527189769" POSITION="right" TEXT="Previous to meeting">
+<node CREATED="1523527043369" ID="Freemind_Link_741110284" MODIFIED="1523527228399" TEXT="Check the email to get the invitation, &#xa;if mail system has calendar service, &#xa;the event will be added."/>
+<node CREATED="1523527112417" ID="Freemind_Link_1524441210" MODIFIED="1523527255027" TEXT="Check Zoom Webinar application,&#xa;if invited, will be added connection&#xa;to the host&#xa;"/>
+</node>
+<node CREATED="1523526988734" ID="Freemind_Link_688249422" MODIFIED="1523526998888" POSITION="right" TEXT="At the time of the meeting">
+<node CREATED="1523527266568" ID="Freemind_Link_1606366875" MODIFIED="1523527290831" TEXT="If Zoom Webinar installed: Open app and click join"/>
+<node CREATED="1523527303540" ID="Freemind_Link_201299977" MODIFIED="1523527320478" TEXT="Else: open the web browser, copy and paste the link to join the meeting"/>
+</node>
+<node CREATED="1523527003613" ID="Freemind_Link_1400322122" MODIFIED="1523527345214" POSITION="right" TEXT="After the meeting: Can view the videos uploaded to Zoom cloud"/>
+</node>
+</map>
